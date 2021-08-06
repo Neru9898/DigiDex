3df44b4ece365b19f3670945db7059aa5c810875
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-08-05 21:44:56 -0400

    Started to created custom dynamic picture slides

--- client/src/Components/Body.js
+++ client/src/Components/Body.js
@@ -20,54 +20,14 @@ export const Body = () => {
   }, []);
   return (
     <>
-      {/* // <ImageList cols={2.5}>
-    //   {digiList.map((item) => (
-    //     <ImageListItem
-    //       key={item.name}
-    //       style={{ width: "100%", height: "auto" }}
-    //     >
-    //       <img
-    //         src={item.img}
-    //         alt={item.name}
-    //         style={{ width: "auto", height: "auto" }}
-    //       />
-    //       <ImageListItemBar title={item.name} />
-    //     </ImageListItem>
-    //   ))}
-    // </ImageList> */}
-      {/* {digiList.map((digimon, i) => (
-        <img src={digimon.img} />
-      ))} */}
-
-      <Carousel
-        NavButton={({ onClick, className, style, next, prev }) => {
-          // Other logic
-
-          return (
-            <Button onClick={onClick}>
-              {next && "Next"}
-              {prev && "Previous"}
-            </Button>
-          );
-        }}
-      >
-        <img src={digiList[0].img} />
-      </Carousel>
-      {/* //   {digiList.map((digimon) => {
-    //     return (
-    //       <Grid
-    //         container
-    //         direction="row"
-    //         justifyContent="space-evenly"
-    //         alignItems="flex-start"
-    //         pacing={1}
-    //       >
-    //         <Typography variant="h3"> {digimon.name}</Typography>
-    //         <img src={digimon.img} alt="digimon.name" />
-    //         <Typography variant="h5"> {digimon.level}</Typography>
-    //       </Grid>
-    //     );
-    //   })} */}
+      {digiList.map((digimon) => {
+        return (
+          <Grid container direction="row" justifyContent="center">
+            <Typography variant="h5">{digimon.name}</Typography>
+            <img src={digimon.img} alt="digimon.name" />
+          </Grid>
+        );
+      })}
     </>
   );
 };
