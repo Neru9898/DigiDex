592c409bfbca57005c72572ce56b1cdd2a62f88b
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-08-10 19:31:13 -0400

    Did minor stlye and created layout

--- client/src/Components/Body.js
+++ client/src/Components/Body.js
@@ -1,33 +1,51 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Carousel from "react-material-ui-carousel";
+import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
+import ArrowBackIosRoundedIcon from "@material-ui/icons/ArrowBackIosRounded";
 import { Button } from "@material-ui/core";
-import {
-  Grid,
-  Typography,
-  ImageList,
-  ImageListItem,
-  ImageListItemBar,
-  Card,
-} from "@material-ui/core";
+import { Grid, Typography, Slide } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 export const Body = () => {
-  const [digiList, setDigiList] = useState([]);
   useEffect(() => {
     axios.get("https://digimon-api.vercel.app/api/digimon").then((res) => {
       setDigiList(res.data);
     });
   }, []);
+
+  const [digiList, setDigiList] = useState([]);
+  const [currentDigimon, setCurrentDigimon] = useState(0);
+
   return (
     <>
-      {digiList.map((digimon) => {
-        return (
-          <Grid container direction="row" justifyContent="center">
-            <Typography variant="h5">{digimon.name}</Typography>
-            <img src={digimon.img} alt="digimon.name" />
+      <Grid
+        container
+        spacing={3}
+        style={{ margin: "50px", position: "relative" }}
+      >
+        <Grid item xs style={{ position: "absolute", marginTop: "20%" }}>
+          <Button>
+            <ArrowBackIosRoundedIcon style={{ borderRadius: "50%" }} />
+          </Button>
+        </Grid>
+        <Slide>
+          <Grid item xs={6} style={{ position: "absolute", padding: "250px" }}>
+            <img
+              src={digiList[currentDigimon].img}
+              alt="Digimon"
+              style={{ height: "40vh", width: "40wh", borderRadius: "50%" }}
+            />
           </Grid>
-        );
-      })}
+        </Slide>
+        <Grid
+          item
+          xs
+          style={{ position: "absolute", marginTop: "20%", marginLeft: "43%" }}
+        >
+          <ArrowForwardIosRoundedIcon />
+        </Grid>
+      </Grid>
     </>
   );
 };
