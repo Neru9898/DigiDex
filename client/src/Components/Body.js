92d0504305a2700833085978599666572566c793
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-08-10 20:35:39 -0400

    Set  up Click function and transition

--- client/src/Components/Body.js
+++ client/src/Components/Body.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowBackIosRoundedIcon from "@material-ui/icons/ArrowBackIosRounded";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import { Button } from "@material-ui/core";
 import { Grid, Typography, Slide } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 export const Body = () => {
+  const [digiList, setDigiList] = useState([]);
+
+  //need to sort out this as image dont render when refreshing
   useEffect(() => {
     axios.get("https://digimon-api.vercel.app/api/digimon").then((res) => {
       setDigiList(res.data);
     });
   }, []);
 
-  const [digiList, setDigiList] = useState([]);
   const [currentDigimon, setCurrentDigimon] = useState(0);
+  const [slideIn, setSlideIn] = useState(true);
+  const [slideDir, setslideDir] = useState("down");
+  const content = digiList[currentDigimon];
+  const numImg = digiList.length;
+
+  console.log(digiList);
+  const onArrowClick = (dir) => {
+    const increment = dir === "left" ? -1 : 1;
+    const newIndex = (currentDigimon + increment + numImg) % numImg;
+    setslideDir(dir);
+    setSlideIn(false);
+
+    setTimeout(() => {
+      setCurrentDigimon(newIndex);
+      setslideDir(dir === "left" ? "right" : "left");
+      setSlideIn(true);
+    }, 700);
+  };
 
   return (
     <>
@@ -24,26 +45,20 @@ export const Body = () => {
         spacing={3}
         style={{ margin: "50px", position: "relative" }}
       >
-        <Grid item xs style={{ position: "absolute", marginTop: "20%" }}>
-          <Button>
-            <ArrowBackIosRoundedIcon style={{ borderRadius: "50%" }} />
-          </Button>
-        </Grid>
-        <Slide>
-          <Grid item xs={6} style={{ position: "absolute", padding: "250px" }}>
+        <Grid item xs>
+          <ArrowBackIosIcon onClick={() => onArrowClick("left")} />
+        </Grid>{" "}
+        <Slide direction={slideDir} in={slideIn}>
+          <Grid item xs={6}>
             <img
               src={digiList[currentDigimon].img}
               alt="Digimon"
-              style={{ height: "40vh", width: "40wh", borderRadius: "50%" }}
+              style={{ height: "40vh", width: "40wh", borderRadius: "40%" }}
             />
           </Grid>
         </Slide>
-        <Grid
-          item
-          xs
-          style={{ position: "absolute", marginTop: "20%", marginLeft: "43%" }}
-        >
-          <ArrowForwardIosRoundedIcon />
+        <Grid item xs>
+          <ArrowForwardIosIcon onClick={() => onArrowClick("right")} />
         </Grid>
       </Grid>
     </>
